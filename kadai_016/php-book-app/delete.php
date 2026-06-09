533570cac06ca43d42d38050c7b1db7d80543ead
k-for-learn <wG3qaKc.tSTcz3sD@gmail.com>
2026-06-09 23:14:07 +0900

    kadai_016 完成形

---
+++ kadai_016/php-book-app/delete.php
@@ -0,0 +1,22 @@
+<?php
+$dsn = 'mysql:dbname=php_book_app;host=localhost;charset=utf8mb4';
+$user = 'root';
+$password = 'root';
+
+if (isset($_GET['id'])) {
+    try {
+        $pdo = new PDO($dsn, $user, $password);
+        $sql_delete = 'DELETE from books where id = :id';
+        $stmt_delete = $pdo->prepare($sql_delete);
+        $stmt_delete->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
+        $stmt_delete->execute();
+
+        $count = $stmt_delete->rowCount();
+        $message = "商品を{$count}件削除しました。";
+        header("Location: read.php?message={$message}");
+    } catch (PDOException $e) {
+        exit($e->getMessage());
+    }
+} else {
+    exit('idパラメータの値が存在しません。');
+}
